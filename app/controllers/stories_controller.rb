0c628e575d6b105c347ac0c8e88e6a70791e3c4e
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-06-16 00:34:30 -0700

    add route new for story controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,3 +1,21 @@
 #inherits methods from ApplicationController (helpers, configuration, etc.)
 class StoriesController < ApplicationController
+
+  get '/stories/new' do
+    erb :'/stories/new'
+  end
+
+  post '/stories' do
+    if !logged_in?
+      redirect '/'
+    end
+
+    if params[:name] != "" && params[:author] != ""
+      @story = Story.create(name: params[:name], author: params[:author], user_id: current_user.id)
+      redirect "/stories/#{@story.id}"
+    else
+      redirect '/stories/new'
+    end
+  end
+
 end
